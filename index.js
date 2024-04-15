a9ebad7e7c3dc6b467d6d69655fc6ba0a6f01be2
AshutoshDM1 <downlodemaster1@gmail.com>
2024-04-15 19:35:29 +0530

    first Gsap project

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 let t1 = gsap.timeline();
 
-// t1.to("#div1" , {
-//   x:400,
-//   duration : 2,
-//   backgroundColor:"#0d91f0",
-//   scale : .8,  
-// })
-
-// t1.to("#div2" , {
-//   x:400,
-//   duration : 2,
-//   backgroundColor:"#0d91f0",
-//   scale : .5,  
-//   rotate :180,
-// })
 t1.from("#nav>h1", {
-  y:-50,
-  duration:.8,
-  delay:0.3,
+  y: -50,
+  duration: 0.8,
+  delay: 0.3,
   opacity: 0,
-  stagger: 0.2, // delay between each animation (in seconds)
+  stagger: 0.2,
 });
 gsap.from("#nav2>h1", {
-  y:-50,
-  duration:.8,
-  delay:0.3,
+  y: -50,
+  duration: 0.8,
+  delay: 0.3,
   opacity: 0,
-  stagger: 0.2, // delay between each animation (in seconds)
+  stagger: 0.2,
+});
+t1.from("hr", {
+  x: -2500,
+  duration: 1,
+  stagger: 0.7,
 });
 
 t1.from(".textBody h1", {
-  x:-1000,
-  y:-200,
-  duration:1,
-  stagger:0.7,
+  x: -1000,
+  y: -200,
+  duration: 1,
+  stagger: 0.7,
+});
+
+gsap.to("#page2 > .img_box >img", {
+  width:"100%",
+  duration: 3,
+  delay: 0.5,
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body" ,
+    markers: true,
+    start : "top -5%",
+    end : "top -35%",
+    scrub: 4,
+    pin:true,
+  },
+});
+
+gsap.to("#page3 > span", {
+  transform: "translateX(-36%)",
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "body" ,
+    markers: true,
+    start : "top -5%",
+    end : "top -35%",
+    scrub: 4,
+    pin:true,
+  },
 })
